2d898a6ec45ef0c18e675798f6d256aa2ea31181
Mark Samways <samways2008@hotmail.co.uk>
2018-10-17 10:15:08 +0100

    Hi

--- JustRipeProjectOfficial/JustRipeProjectOfficial/Program.cs
+++ JustRipeProjectOfficial/JustRipeProjectOfficial/Program.cs
@@ -16,7 +16,8 @@ namespace JustRipeProjectOfficial
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); 
+            // Mark is here
         }
     }
 }
