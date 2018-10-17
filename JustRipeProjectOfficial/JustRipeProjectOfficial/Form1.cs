64d0c7b2f97ef3703c83a1881851c96cbfc759a1
Thomas Jenkins <TJ296@student.anglia.ac.uk>
2018-10-17 10:43:29 +0100

    [Form1.cs] Tom coded

--- JustRipeProjectOfficial/JustRipeProjectOfficial/Form1.cs
+++ JustRipeProjectOfficial/JustRipeProjectOfficial/Form1.cs
@@ -16,5 +16,10 @@ namespace JustRipeProjectOfficial
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // tom is here.
+        }
     }
 }
