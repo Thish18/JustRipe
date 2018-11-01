30b7884dc02ef5f8fa74c8aa749bcc72eed6e522
Wilson Ng <chonlam88@gmail.com>
2018-11-01 18:45:41 +0000

    dont even try to run the program xD Currently none of the function from DBConnect works

--- JustRipeProjectOfficial/JustRipeProjectOfficial/DBConnect.cs
+++ JustRipeProjectOfficial/JustRipeProjectOfficial/DBConnect.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 //use for messagebox in this case.
 using System.Windows.Forms;
 //use for mysql commands and functions.
-using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
 
 namespace JustRipeProjectOfficial
 {
@@ -15,79 +15,38 @@ namespace JustRipeProjectOfficial
     {
 
         /*===============================================================[Database Connection Setup]===============================================================================================*/
-        /*Setup for connection to the MYSQL Server*/
-        private MySqlConnection conn;
-
-        private string DBConnStr;
-        //fill in information needed for connection.
-        private string server = "localhost";
-        private string database = "mydb";
-        private string uid = "root";
-        private string password = "";
-
-        //solve the server doesnt support ssl connection error.
-        private string sslmode = "none";
 
         //setup logged in info.
         private int userID;
 
-        /*Using information given above to fill in the gaps in connection string
-         the use it for the actual SQL connection query.*/
+        private string conn;
+        SqlConnection connToDB;
+        private SqlDataAdapter dataAdap;
 
-        public void Initialize()
+        public DBConnect(string connStr)
         {
-            string connectionString = Properties.Settings.Default.DBJustRipeConnStr;
 
-
-
-            //using information given above.
-            string connStr = "Server=" + server + ";database=" + database +
-                ";UID=" + uid + ";Password=" + password + ";" + "sslmode=" + sslmode + ";";
-
-            //connection query for SQL.
-            conn = new MySqlConnection(connStr);
+            conn = connStr;
 
         }
 
         //once MYSQL Connected. I believe this will actually try to connect to the database server.
-        public bool OpenConn()
+        public void OpenConn()
         {
 
-            try
-            {
+            connToDB = new SqlConnection(conn);
 
-                conn.Open();
-                return true;
-
-            }
-            catch (MySqlException ex)
-            {
-
-                MessageBox.Show(ex.Message);
-                return false;
-
-            }
+            connToDB.Open();
 
         }
 
         //this will close the connection to the database server once the program closed.
-        /*
-        public bool CloseConn() {
+        
+        public void CloseConn() {
 
-            try {
+            connToDB.Close();
 
-                conn.Close();
-                return true;
-
-            }
-            catch (MySqlException ex) {
-
-                MessageBox.Show(ex.Message);
-                return false;
-
-            }
-
-        }*/
+        }
 
 
         /*===============================================================[LogIn/Register Functions]===============================================================================================*/
@@ -96,35 +55,23 @@ namespace JustRipeProjectOfficial
         {
 
             string dataUN = "";
-            Initialize();
-            conn.Open();
+
 
             string query = "SELECT username FROM users WHERE username = '" + un + "'";
-            MySqlCommand cmd = new MySqlCommand(query, conn);
 
-            /*important code to export data read from sql database.*/
-            MySqlDataReader mdr = cmd.ExecuteReader();
-
-
-
-            while (mdr.Read())
-            {
-
-                dataUN = (string)mdr["username"];
-
-            }
+            /*Missing functions*/
 
             if (dataUN == un)
             {
 
-                conn.Close();
+
                 return true;
 
             }
             else
             {
 
-                conn.Close();
+
                 return false;
 
             }
@@ -140,25 +87,9 @@ namespace JustRipeProjectOfficial
             string dataUN = "";
             string datapw = "";
 
-            Initialize();
-            conn.Open();
-
             string query = "SELECT users_ID, username, Password FROM users WHERE username = '" + un + "'";
-            MySqlCommand cmd = new MySqlCommand(query, conn);
 
-            /*important code to export data read from sql database.*/
-            MySqlDataReader mdr = cmd.ExecuteReader();
-
-            while (mdr.Read())
-            {
-
-                //use to check username and password.
-                dataUN = (string)mdr["username"];
-                datapw = (string)mdr["Password"];
-                //userID to temp store user ID.
-                userID = (int)mdr["users_ID"];
-
-            }
+            /*Missing Functions*/
 
             if (datapw == pw)
             {
@@ -183,56 +114,18 @@ namespace JustRipeProjectOfficial
         public void userInfoImport()
         {
 
-            Initialize();
-            conn.Open();
-
             string query = "SELECT users.*, ranktype.* FROM users INNER JOIN ranktype ON users.rankID = ranktype.rank_ID WHERE users_ID = '" + userID + "';";
-            MySqlCommand cmd = new MySqlCommand(query, conn);
 
-            /*important code to export data read from sql database.*/
-            MySqlDataReader mdr = cmd.ExecuteReader();
-
-            while (mdr.Read())
-            {
-
-                //storing all users data to the temp list.
-                tempUserInfo.Add((int)mdr["users_ID"]);
-                tempUserInfo.Add((string)mdr["firstname"]);
-                tempUserInfo.Add((string)mdr["lastname"]);
-                tempUserInfo.Add((string)mdr["username"]);
-                tempUserInfo.Add((string)mdr["Password"]);
-                tempUserInfo.Add((string)mdr["ContactNum"]);
-                tempUserInfo.Add((string)mdr["rankType"]);
-
-            }
+            /*Missing Functions*/
 
         }
         //export ONLY
         public void userInfoExport(int userID)
         {
 
-            Initialize();
-            conn.Open();
-
             string query = "SELECT users.*, ranktype.* FROM users INNER JOIN ranktype ON users.rankID = ranktype.rank_ID WHERE users_ID = " + userID + "";
-            MySqlCommand cmd = new MySqlCommand(query, conn);
 
-            /*important code to export data read from sql database.*/
-            MySqlDataReader mdr = cmd.ExecuteReader();
-
-            while (mdr.Read())
-            {
-
-                //storing all users data to the temp list.
-                tempUserInfo.Add((int)mdr["users_ID"]);
-                tempUserInfo.Add((string)mdr["firstname"]);
-                tempUserInfo.Add((string)mdr["lastname"]);
-                tempUserInfo.Add((string)mdr["username"]);
-                tempUserInfo.Add((string)mdr["Password"]);
-                tempUserInfo.Add((string)mdr["ContactNum"]);
-                tempUserInfo.Add((string)mdr["rankType"]);
-
-            }
+            /*Missing Functions*/
 
         }
 
@@ -241,25 +134,13 @@ namespace JustRipeProjectOfficial
         public void userCreate(string fn, string ln, string dob, string g, string ad1, string ad2, string no, string un, string pw)
         {
 
-            Initialize();
-            conn.Open();
-
             //connection query for SQL.
             string connstr = "INSERT INTO users(firstname, lastname,username,Password,ContactNum,rankID) VALUES(@firstname, @lastname,@username,@Password,@ContactNum,@rankID)";
 
-            MySqlCommand comm = new MySqlCommand(connstr, conn);
-            //comm.CommandText = "INSERT INTO users(firstname, lastname,username,Password,ContactNum,rankID) VALUES(@firstname, @lastname,@username,@Password,@ContactNum,@rankID)";
-            comm.Parameters.AddWithValue("@firstname", fn);
-            comm.Parameters.AddWithValue("@lastname", ln);
-            comm.Parameters.AddWithValue("@username", un);
-            comm.Parameters.AddWithValue("@Password", pw);
-            comm.Parameters.AddWithValue("@ContactNum", no);
-            comm.Parameters.AddWithValue("@rankID", "1");
-            comm.ExecuteNonQuery();
+            /*Missing Functions*/
 
             MessageBox.Show("User Created.");
 
-            conn.Close();
 
         }
 
@@ -297,27 +178,6 @@ namespace JustRipeProjectOfficial
         private List<object[]> labourerList = new List<object[]> { };
         public void getLabourersList() {
 
-            Initialize();
-            conn.Open();
-
-            string query = "SELECT  users_ID, firstname, lastname FROM users;";
-            MySqlCommand cmd = new MySqlCommand(query, conn);
-
-            MySqlDataReader mdr = cmd.ExecuteReader();
-
-            object[] labourerListInfo = new object[3];
-            while (mdr.Read())
-            {
-
-                labourerListInfo[0] = (int)mdr["users_ID"];
-                labourerListInfo[1] = (string)mdr["firstname"];
-                labourerListInfo[2] = (string)mdr["lastname"];
-
-                
-
-            }
-
-            labourerList.Add(labourerListInfo);
 
         }
 
