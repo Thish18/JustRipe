a7ba45af847fc011e0e178dfc56ee80cd2617266
Thish18 <tb1808@gmail.com>
2018-11-12 11:35:31 +0000

    query code for change password, testing the code noew to see if it works

--- JustRipeProjectOfficial/JustRipeProjectOfficial/ChangePassword.cs
+++ JustRipeProjectOfficial/JustRipeProjectOfficial/ChangePassword.cs
@@ -7,32 +7,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace JustRipeProjectOfficial
 {
     public partial class ChangePassword : Form
     {
+
+        private string connStr;
+        SqlConnection connToDB;
+        private SqlDataAdapter dataAdap;
+        private SqlDataReader dataRead;
+        SqlCommand comm;
+
         public ChangePassword()
         {
             InitializeComponent();
         }
 
+        public void OpenConn()
+        {
+            connToDB = new SqlConnection(connStr);        
+            connToDB.Open();
+        }
+        //this will close the connection to the database server once the program closed
+        public void CloseConn()
+        {
+            connToDB.Close();
+        }
+ /// <summary>
+ /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ /// </summary>
+        static BindingSource bs;
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            OpenConn();
             DBConnect dbconn = new DBConnect();
 
-            if (dbconn.unCheckExist(txtUsername.Text).Equals(true))
+            if (txtUsername.Text != "" && txtPassword.Text != "" && txtNewPassword.Text != "" && txtConfirmPassword.Text != "")
             {
+                
+                string query = "SELECT Users.firstname = Users.Password " +
+                "FROM Users " +
+                "WHERE Password = @UserEnteredPassword ";
+                comm = new SqlCommand(query, connToDB);
+                comm.Parameters.Add("@UserEnteredPassword", SqlDbType.VarChar).Value = txtPassword.Text;
+                dataAdap = new SqlDataAdapter(query, connToDB);
+                DataTable dt = new DataTable();
+                dataAdap.Fill(dt);
+                bs = new BindingSource(dt, null);
+                comm.ExecuteNonQuery();
 
-                MessageBox.Show("yah");
+                if (dt.Rows.Count == 1) //Old Password is correct.
+                {
+                    if (txtNewPassword.Text == txtConfirmPassword.Text) //Checking that the new password was typed correctly.
+                    {
+                        DialogResult Answer = MessageBox.Show("Are you sure you wish to change your current login password?", "Password change confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (Answer == DialogResult.Yes)
+                        {
+                            string query2 = "UPDATE [User] " +
+                            "SET [Password] = @NewPassword " +
+                            "WHERE [Users].[firstname] = @firstname";
+                            comm = new SqlCommand(query, connToDB);
 
+                            comm.Parameters.Add("@NewPassword", SqlDbType.VarChar).Value = txtNewPassword.Text;
+                            comm.Parameters.Add("@firstname", SqlDbType.VarChar).Value = txtUsername.Text;
+                            comm.ExecuteNonQuery();
+
+                            MessageBox.Show("The password has been changed.", "Password changed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Hide();
+                            login frm = new login();
+                            frm.ShowDialog();
+                        }
+                        else
+                        {
+                            MessageBox.Show("The password has not been changed.", "Password not changed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("The new password may have been typed incorrectly.", "Password change failure", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else //Old password isn't correct.
+                {
+                    MessageBox.Show("The old password you entered is incorrect.", "Password change failure", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
             }
-            else {
-
-                MessageBox.Show("Incorrect username/password.");
-
-
+            else
+            {
+                MessageBox.Show("Not all required fields have data in them.", "Password change failure", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+            CloseConn();
+            
+            //if (dbconn.unCheckExist(txtUsername.Text).Equals(true))
+            //{
+            //    MessageBox.Show("yah");
+            //}
+            //else {
+            //    MessageBox.Show("Incorrect username/password.");
+        
+
+            //}
 
         }
 
