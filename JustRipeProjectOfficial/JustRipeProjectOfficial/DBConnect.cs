ff0996eef1d9eb6883fffd171984e81b8b6cb2ff
Mark Samways <samways2008@hotmail.co.uk>
2018-11-14 10:33:05 +0000

    changes and new tables

--- JustRipeProjectOfficial/JustRipeProjectOfficial/DBConnect.cs
+++ JustRipeProjectOfficial/JustRipeProjectOfficial/DBConnect.cs
@@ -391,7 +391,7 @@ namespace JustRipeProjectOfficial
             //connection query for SQL.
             OpenConn();
 
-            string query = "";
+            string query = "SELECT crops.*, workschedule.* FROM crops INNER JOIN workschedule ON crops.crops_ID = workschedule.crops_ID WHERE crops_ID!=''";
 
             comm = new SqlCommand(query, connToDB);
             dataRead = comm.ExecuteReader();
