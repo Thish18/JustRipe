86f2c6623abd1050267137a8eb1d2dceb46e396a
Mark Samways <samways2008@hotmail.co.uk>
2018-11-27 16:00:35 +0000

    create vehicle on form method

--- JustRipeProjectOfficial/JustRipeProjectOfficial/Vehicles.Designer.cs
+++ JustRipeProjectOfficial/JustRipeProjectOfficial/Vehicles.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.btnUpdate = new System.Windows.Forms.Button();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.label15 = new System.Windows.Forms.Label();
+            this.txtFuelType = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPlateNumber = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel5 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -39,12 +45,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnMenu = new System.Windows.Forms.Button();
             this.lBVehiclesList = new System.Windows.Forms.ListBox();
-            this.label15 = new System.Windows.Forms.Label();
-            this.txtFuelType = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.txtPlateNumber = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.panel6.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -58,12 +58,14 @@
             this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.ForeColor = System.Drawing.Color.White;
-            this.btnUpdate.Location = new System.Drawing.Point(1, 387);
+            this.btnUpdate.Location = new System.Drawing.Point(1, 476);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(248, 54);
+            this.btnUpdate.Size = new System.Drawing.Size(331, 66);
             this.btnUpdate.TabIndex = 8;
             this.btnUpdate.Text = "Update Information";
             this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // panel6
             // 
@@ -75,18 +77,74 @@
             this.panel6.Controls.Add(this.label4);
             this.panel6.Controls.Add(this.btnUpdate);
             this.panel6.Controls.Add(this.panel5);
-            this.panel6.Location = new System.Drawing.Point(233, 9);
+            this.panel6.Location = new System.Drawing.Point(311, 11);
+            this.panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(248, 441);
+            this.panel6.Size = new System.Drawing.Size(331, 543);
             this.panel6.TabIndex = 14;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(29, 188);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(79, 17);
+            this.label15.TabIndex = 22;
+            this.label15.Text = "Fuel Type: ";
+            // 
+            // txtFuelType
+            // 
+            this.txtFuelType.Location = new System.Drawing.Point(109, 185);
+            this.txtFuelType.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFuelType.Name = "txtFuelType";
+            this.txtFuelType.Size = new System.Drawing.Size(173, 22);
+            this.txtFuelType.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Vehicle: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(109, 85);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(173, 22);
+            this.txtName.TabIndex = 19;
+            // 
+            // txtPlateNumber
+            // 
+            this.txtPlateNumber.Location = new System.Drawing.Point(109, 132);
+            this.txtPlateNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPlateNumber.Name = "txtPlateNumber";
+            this.txtPlateNumber.Size = new System.Drawing.Size(173, 22);
+            this.txtPlateNumber.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Plate No: ";
             // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(39)))), ((int)(((byte)(53)))));
             this.panel5.Controls.Add(this.label3);
             this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(544, 54);
+            this.panel5.Size = new System.Drawing.Size(725, 66);
             this.panel5.TabIndex = 11;
             // 
             // label3
@@ -94,9 +152,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(12, 11);
+            this.label3.Location = new System.Drawing.Point(16, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(237, 29);
+            this.label3.Size = new System.Drawing.Size(289, 36);
             this.label3.TabIndex = 2;
             this.label3.Text = "Vehicle Information";
             // 
@@ -109,8 +168,9 @@
             this.panel1.Controls.Add(this.lBVehiclesList);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(227, 459);
+            this.panel1.Size = new System.Drawing.Size(303, 565);
             this.panel1.TabIndex = 15;
             // 
             // btnSelect
@@ -120,9 +180,10 @@
             this.btnSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSelect.ForeColor = System.Drawing.Color.White;
-            this.btnSelect.Location = new System.Drawing.Point(0, 264);
+            this.btnSelect.Location = new System.Drawing.Point(0, 325);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSelect.Name = "btnSelect";
-            this.btnSelect.Size = new System.Drawing.Size(227, 65);
+            this.btnSelect.Size = new System.Drawing.Size(303, 80);
             this.btnSelect.TabIndex = 8;
             this.btnSelect.Text = "Select";
             this.btnSelect.UseVisualStyleBackColor = false;
@@ -135,20 +196,23 @@
             this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCreate.ForeColor = System.Drawing.Color.White;
-            this.btnCreate.Location = new System.Drawing.Point(0, 329);
+            this.btnCreate.Location = new System.Drawing.Point(0, 405);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(227, 65);
+            this.btnCreate.Size = new System.Drawing.Size(303, 80);
             this.btnCreate.TabIndex = 7;
             this.btnCreate.Text = "Create New";
             this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(78)))), ((int)(((byte)(89)))));
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(227, 52);
+            this.panel2.Size = new System.Drawing.Size(303, 64);
             this.panel2.TabIndex = 5;
             // 
             // label1
@@ -156,9 +220,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 29);
+            this.label1.Size = new System.Drawing.Size(155, 36);
             this.label1.TabIndex = 1;
             this.label1.Text = "Vehicles: ";
             // 
@@ -169,9 +234,10 @@
             this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenu.ForeColor = System.Drawing.Color.White;
-            this.btnMenu.Location = new System.Drawing.Point(0, 394);
+            this.btnMenu.Location = new System.Drawing.Point(0, 485);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMenu.Name = "btnMenu";
-            this.btnMenu.Size = new System.Drawing.Size(227, 65);
+            this.btnMenu.Size = new System.Drawing.Size(303, 80);
             this.btnMenu.TabIndex = 6;
             this.btnMenu.Text = "Back to main menu";
             this.btnMenu.UseVisualStyleBackColor = false;
@@ -183,68 +249,23 @@
             this.lBVehiclesList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lBVehiclesList.ForeColor = System.Drawing.Color.White;
             this.lBVehiclesList.FormattingEnabled = true;
-            this.lBVehiclesList.Location = new System.Drawing.Point(0, 52);
+            this.lBVehiclesList.ItemHeight = 17;
+            this.lBVehiclesList.Location = new System.Drawing.Point(0, 64);
+            this.lBVehiclesList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lBVehiclesList.Name = "lBVehiclesList";
-            this.lBVehiclesList.Size = new System.Drawing.Size(227, 212);
+            this.lBVehiclesList.Size = new System.Drawing.Size(301, 259);
             this.lBVehiclesList.TabIndex = 4;
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(22, 153);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(60, 13);
-            this.label15.TabIndex = 22;
-            this.label15.Text = "Fuel Type: ";
-            // 
-            // txtFuelType
-            // 
-            this.txtFuelType.Location = new System.Drawing.Point(82, 150);
-            this.txtFuelType.Name = "txtFuelType";
-            this.txtFuelType.Size = new System.Drawing.Size(131, 20);
-            this.txtFuelType.TabIndex = 23;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 72);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 13);
-            this.label2.TabIndex = 18;
-            this.label2.Text = "Vehicle: ";
-            // 
-            // txtName
-            // 
-            this.txtName.Location = new System.Drawing.Point(82, 69);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(131, 20);
-            this.txtName.TabIndex = 19;
-            // 
-            // txtPlateNumber
-            // 
-            this.txtPlateNumber.Location = new System.Drawing.Point(82, 107);
-            this.txtPlateNumber.Name = "txtPlateNumber";
-            this.txtPlateNumber.Size = new System.Drawing.Size(131, 20);
-            this.txtPlateNumber.TabIndex = 21;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 110);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 13);
-            this.label4.TabIndex = 20;
-            this.label4.Text = "Plate No: ";
             // 
             // Vehicles
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(233)))), ((int)(((byte)(243)))));
-            this.ClientSize = new System.Drawing.Size(490, 459);
+            this.ClientSize = new System.Drawing.Size(653, 565);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel6);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Vehicles";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Vehicles:";
