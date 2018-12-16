043ef3db30324e938860f5d074aeb93bc9f7a32b
Mark Samways <samways2008@hotmail.co.uk>
2018-12-16 01:41:30 +0000

    sowing update

--- JustRipeProjectOfficial/JustRipeProjectOfficial/sowingMethod.Designer.cs
+++ JustRipeProjectOfficial/JustRipeProjectOfficial/sowingMethod.Designer.cs
@@ -45,6 +45,8 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timeNeededTextBox = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel6.SuspendLayout();
@@ -59,7 +61,7 @@
             this.lBSowingList.FormattingEnabled = true;
             this.lBSowingList.ItemHeight = 17;
             this.lBSowingList.Location = new System.Drawing.Point(0, 64);
-            this.lBSowingList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lBSowingList.Margin = new System.Windows.Forms.Padding(4);
             this.lBSowingList.Name = "lBSowingList";
             this.lBSowingList.Size = new System.Drawing.Size(301, 259);
             this.lBSowingList.TabIndex = 4;
@@ -89,7 +91,7 @@
             // maxQuantityTextBox
             // 
             this.maxQuantityTextBox.Location = new System.Drawing.Point(255, 122);
-            this.maxQuantityTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.maxQuantityTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.maxQuantityTextBox.Name = "maxQuantityTextBox";
             this.maxQuantityTextBox.Size = new System.Drawing.Size(109, 22);
             this.maxQuantityTextBox.TabIndex = 16;
@@ -97,7 +99,7 @@
             // quantityTextBox
             // 
             this.quantityTextBox.Location = new System.Drawing.Point(96, 122);
-            this.quantityTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.quantityTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.quantityTextBox.Name = "quantityTextBox";
             this.quantityTextBox.Size = new System.Drawing.Size(125, 22);
             this.quantityTextBox.TabIndex = 15;
@@ -115,7 +117,7 @@
             // typeTextBox
             // 
             this.typeTextBox.Location = new System.Drawing.Point(96, 86);
-            this.typeTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.typeTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.typeTextBox.Name = "typeTextBox";
             this.typeTextBox.Size = new System.Drawing.Size(268, 22);
             this.typeTextBox.TabIndex = 13;
@@ -138,7 +140,7 @@
             this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSelect.ForeColor = System.Drawing.Color.White;
             this.btnSelect.Location = new System.Drawing.Point(0, 325);
-            this.btnSelect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(4);
             this.btnSelect.Name = "btnSelect";
             this.btnSelect.Size = new System.Drawing.Size(303, 80);
             this.btnSelect.TabIndex = 8;
@@ -154,7 +156,7 @@
             this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCreate.ForeColor = System.Drawing.Color.White;
             this.btnCreate.Location = new System.Drawing.Point(0, 405);
-            this.btnCreate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(4);
             this.btnCreate.Name = "btnCreate";
             this.btnCreate.Size = new System.Drawing.Size(303, 80);
             this.btnCreate.TabIndex = 7;
@@ -167,7 +169,7 @@
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(78)))), ((int)(((byte)(89)))));
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(303, 64);
             this.panel2.TabIndex = 5;
@@ -180,7 +182,7 @@
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.ForeColor = System.Drawing.Color.White;
             this.btnUpdate.Location = new System.Drawing.Point(0, 473);
-            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(375, 66);
             this.btnUpdate.TabIndex = 8;
@@ -208,7 +210,7 @@
             this.btnMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenu.ForeColor = System.Drawing.Color.White;
             this.btnMenu.Location = new System.Drawing.Point(0, 485);
-            this.btnMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(4);
             this.btnMenu.Name = "btnMenu";
             this.btnMenu.Size = new System.Drawing.Size(303, 80);
             this.btnMenu.TabIndex = 6;
@@ -225,13 +227,15 @@
             this.panel1.Controls.Add(this.lBSowingList);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(303, 565);
             this.panel1.TabIndex = 17;
             // 
             // panel6
             // 
+            this.panel6.Controls.Add(this.timeNeededTextBox);
+            this.panel6.Controls.Add(this.label6);
             this.panel6.Controls.Add(this.label5);
             this.panel6.Controls.Add(this.maxQuantityTextBox);
             this.panel6.Controls.Add(this.quantityTextBox);
@@ -241,7 +245,7 @@
             this.panel6.Controls.Add(this.btnUpdate);
             this.panel6.Controls.Add(this.panel5);
             this.panel6.Location = new System.Drawing.Point(313, 11);
-            this.panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel6.Margin = new System.Windows.Forms.Padding(4);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(375, 539);
             this.panel6.TabIndex = 16;
@@ -251,10 +255,28 @@
             this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(39)))), ((int)(((byte)(53)))));
             this.panel5.Controls.Add(this.label3);
             this.panel5.Location = new System.Drawing.Point(0, 0);
-            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(725, 66);
             this.panel5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 165);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Time Needed: ";
+            // 
+            // timeNeededTextBox
+            // 
+            this.timeNeededTextBox.Location = new System.Drawing.Point(128, 162);
+            this.timeNeededTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.timeNeededTextBox.Name = "timeNeededTextBox";
+            this.timeNeededTextBox.Size = new System.Drawing.Size(236, 22);
+            this.timeNeededTextBox.TabIndex = 19;
             // 
             // sowingMethod
             // 
@@ -264,7 +286,7 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel6);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "sowingMethod";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "sowingMethod";
@@ -298,5 +320,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox timeNeededTextBox;
+        private System.Windows.Forms.Label label6;
     }
 }
